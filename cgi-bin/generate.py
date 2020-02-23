c9118891c74c565c5bf88b5c0f33a23a837a5fb1
Irakli Goderdzishvili <irakli.goder@gmail.com>
2020-02-23 21:28:50 +0100

    Iteration.

---
+++ cgi-bin/generate.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import cgitb
+import cgi
+cgitb.enable()
+
+print("Content-Type: text/html")
+print()
+
+# Get input from user
+form = cgi.FieldStorage()
+user_color = form["color"].value.upper()
+
+# Read text file
+f = open('./src/html_colors.txt')
+try:
+	# Create an array of all the colors
+	colors = f.read().splitlines()
+finally:
+    f.close()
+
+# Print HTML
+if user_color in colors:
+	print(f'''<!DOCTYPE html>
+	<html>
+	<head>
+		<title>Color Check</title>
+		<link rel="stylesheet" type="text/css" href="/style.css">
+	</head>
+	<body>
+		<div id="main">
+			<h2><span style="font-family: 'JetBrains Mono'; font-weight: 400; color:#4A7768">{form["color"].value}</span> is a color!</h2>
+			<h3>Here's what it looks like:</h3>
+			<div style="background-color: {form["color"].value}"id="color_block"></div>
+			<a href="/index.html"><button id="back_button">Go back</button></a>
+		</div>
+		<script> </script>
+	</body></html>''')
+else:
+	print(f'''<!DOCTYPE html>
+	<html>
+	<head>
+		<title>Try again</title>
+		<link rel="stylesheet" type="text/css" href="/style.css">
+	</head>
+	<body>
+		<div id="main">
+			<h1><a href="/index.html">Color check</a></h1>
+			<h2>Unfortunately, <span style="font-family: 'JetBrains Mono'; font-weight: 400; color:#4A7768">{form["color"].value}</span> is not a color.</h2>
+			<h3>You can try again here:</h3>
+			<form action="/cgi-bin/generate.py" method="GET">
+				<input autocomplete="off" type="text" name="color" id="color_input" placeholder="'red', '#fa32bc', etc" required>
+				<button id="check_button" type="submit">Check color</button>
+			</form>
+		</div>
+		<script> </script>
+	</body>
+	</html>''')
+
+
+
---
+++ test.py
@@ -0,0 +1,5 @@
+my_var = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+for each_var in my_var:
+	print(each_var)
